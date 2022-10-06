6a9e12f95975929f2a4514e9fb8c0eb9ae51074c
Darya_Savitskaya <statusqvogerz@gmail.com>
2022-10-06 17:47:33 +0500

    Lesson 5 Task 1

---
+++ index.js
@@ -0,0 +1,8 @@
+const myName = 'Савицкая Дарья'
+const programmingLanguage = 'JavaScript'
+const courseCreatorName = 'Владилена Минина'
+const reasonText = `для меня это возможность совместить навыки по программированию с дизайнерскими навыками при front-end разработке (создание веб-сайтов)`
+const numberOfMonth = 3
+
+let resultText = `"Всем привет! Меня зовут ${myName}. Сейчас я изучаю язык программирования ${programmingLanguage} на курсе по ${programmingLanguage} у ${courseCreatorName}. Я хочу стать веб-разработчиком, потому что ${reasonText}. До этого я изучала ${programmingLanguage} ${numberOfMonth} месяцев. Я уверена, что пройду данный курс до конца."`
+console.log(resultText)
